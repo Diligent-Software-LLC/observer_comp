0f1783b6fd18c8f8a1a464301b87928a505be0b3
Bradley J. Tannor <bradleytannor@gmail.com>
2020-05-15 06:40:33 -0700

    Development Complete
    Added observer_comp.rb. Added file tests. Added node dependency.

--- lib/observer_comp.rb
+++ lib/observer_comp.rb
@@ -2,10 +2,12 @@
 # under the GNU General Public License, Version 3. Refer LICENSE.txt.
 
 require_relative "observer_comp/version"
+require 'observer_int'
+require 'observer_impl'
 
 # ObserverComposition.
 # @class_description
-#   An Observer library's Observer composition. Composites the Observer Interface and Implementation.
+#   An Observer library's Observer composition. Composites the Observer
+#   Interface and Implementation.
 class ObserverComposition
 end
-
--- observer_comp.gemspec
+++ observer_comp.gemspec
@@ -44,5 +44,6 @@ are available at $1, $2, $4, $8, and $16. Greatly appreciated.}
   spec.add_development_dependency 'simplecov', '~> 0'
   spec.add_development_dependency 'observer_int', '~> 0'
   spec.add_development_dependency 'observer_impl', '~> 0'
+  spec.add_development_dependency 'node', '~> 4'
 
 end
